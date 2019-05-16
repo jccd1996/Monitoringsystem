621cfb6ee897257ece615e4496cb2afe0e1f23c5
Juan Camilo Cubillos Diaz <juan.cubillos@sundevs.com>
2019-05-16 14:57:42 -0500

    Finish view in main activity

--- app/src/main/java/com/jccd/monitoringsystem/ui/main/MainActivity.kt
+++ app/src/main/java/com/jccd/monitoringsystem/ui/main/MainActivity.kt
@@ -3,8 +3,6 @@ package com.jccd.monitoringsystem.ui.main
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.core.view.GravityCompat
 import androidx.appcompat.app.ActionBarDrawerToggle
 import android.view.MenuItem
@@ -14,42 +12,57 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.jccd.monitoringsystem.R
 import com.jccd.monitoringsystem.db.model.User
 import com.jccd.monitoringsystem.ui.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.nav_header_main.view.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, IMainMVP.view  {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, IMainMVP.view {
 
     private lateinit var presenter: IMainMVP.presenter
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navOptions: NavOptions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         presenter = MainPresenter(this)
         presenter.validateLogUser()
+
         val user = presenter.loadDataUser()
         setDataToNavDrawer(user)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true);
+        supportActionBar!!.setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
+        navController = Navigation.findNavController(this, R.id.fragment_container_main)
+
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+
+
+        // NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+
+        NavigationUI.setupWithNavController(navView, navController)
 
         navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
         } else {
@@ -64,18 +77,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         finish()
     }
 
-    override fun getMainActivity(): Activity  = this
+    override fun getMainActivity(): Activity = this
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> {
                 return true
@@ -86,18 +95,47 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
+
         when (item.itemId) {
             R.id.nav_temperature -> {
-                // Handle the camera action
+                if (navController.currentDestination!!.id != R.id.temperatureFragment) {
+                    navController.popBackStack(R.id.temperatureFragment, true)
+                    navController.navigate(
+                        R.id.temperatureFragment,
+                        null,
+                        destinationFragment(navController.currentDestination!!.id)
+                    )
+                }
             }
             R.id.nav_level_water -> {
-
+                if (navController.currentDestination!!.id != R.id.waterLevelFragment) {
+                    navController.popBackStack(R.id.waterLevelFragment, true)
+                    navController.navigate(
+                        R.id.waterLevelFragment,
+                        null,
+                        destinationFragment(navController.currentDestination!!.id)
+                    )
+                }
             }
             R.id.nav_level_ph -> {
-
+                if (navController.currentDestination!!.id != R.id.phFragment) {
+                    navController.popBackStack(R.id.phFragment, true)
+                    navController.navigate(
+                        R.id.phFragment,
+                        null,
+                        destinationFragment(navController.currentDestination!!.id)
+                    )
+                }
             }
-            R.id.nav_tools -> {
-
+            R.id.nav_history -> {
+                if (navController.currentDestination!!.id != R.id.historyFragment) {
+                    navController.popBackStack(R.id.historyFragment, true)
+                    navController.navigate(
+                        R.id.historyFragment,
+                        null,
+                        destinationFragment(navController.currentDestination!!.id)
+                    )
+                }
             }
             R.id.nav_archive -> {
 
@@ -105,7 +143,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.id.nav_download -> {
 
             }
-            R.id.nav_log_out ->{
+            R.id.nav_log_out -> {
                 presenter.closeSession()
                 return true
             }
@@ -115,10 +153,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-    fun setDataToNavDrawer(user: User){
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    fun setDataToNavDrawer(user: User) {
         val navHead = findViewById<View>(R.id.nav_view) as NavigationView
         val head = navHead.getHeaderView(0)
         head.tvName.text = user.fullName
         head.tvEmail.text = user.email
+    }
+
+    fun destinationFragment(destination: Int): NavOptions {
+        navOptions = NavOptions.Builder()
+            .setPopUpTo(destination, true)
+            .build()
+        return navOptions
     }
 }
