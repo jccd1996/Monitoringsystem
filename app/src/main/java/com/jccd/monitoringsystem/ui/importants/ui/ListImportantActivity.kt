3f61d7d424761b36eb1e28283064e00e21fab592
Juan Camilo Cubillos Diaz <juan.cubillos@sundevs.com>
2019-05-20 21:10:20 -0500

    Create ImportantListActivity

---
+++ app/src/main/java/com/jccd/monitoringsystem/ui/importants/ui/ListImportantActivity.kt
@@ -0,0 +1,13 @@
+package com.jccd.monitoringsystem.ui.importants.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jccd.monitoringsystem.R
+
+class ListImportantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_important)
+    }
+}
